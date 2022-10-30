e8ee5251db1f7d5cfe08e6d23b867bce339a8493
Cong <congusbongus@gmail.com>
2022-10-30 22:28:38 +1100

    Build carts with tic-80

---
+++ .github/workflows/build-carts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+declare -a exts=("lua" "moon" "wren" "js" "fnl")
+
+for example in examples; do
+    for ext in "${exts[@]}"; do
+        cart="${example}/src.${ext}"
+        if [ -f $cart ]; do
+
+            tic80 --cmd="load ${cart} & export html ${example}/${ext}.html & exit" --fs=. --cli
+            unzip ${example}/${ext}.html.zip
+            rm ${example}/${ext}.html.zip
+        fi
+    done
+done
