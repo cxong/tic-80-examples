7377a963872f9fde3bd5abe1b89e6bab68cf9ff6
Cong <congusbongus@gmail.com>
2022-10-30 22:35:53 +1100

    Update build-carts.sh

--- .github/workflows/build-carts.sh
+++ .github/workflows/build-carts.sh
@@ -2,11 +2,10 @@
 
 declare -a exts=("lua" "moon" "wren" "js" "fnl")
 
-for example in examples; do
+for example in examples/*; do
     for ext in "${exts[@]}"; do
         cart="${example}/src.${ext}"
-        if [ -f $cart ]; do
-
+        if [ -f "$cart" ]; then
             tic80 --cmd="load ${cart} & export html ${example}/${ext}.html & exit" --fs=. --cli
             unzip ${example}/${ext}.html.zip
             rm ${example}/${ext}.html.zip
