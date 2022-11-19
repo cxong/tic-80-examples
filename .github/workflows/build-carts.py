15c812b90d9dd8f5046cb1c6de625a4437ab04f3
Cong <congusbongus@gmail.com>
2022-11-19 18:09:09 +1100

    Python build script

---
+++ .github/workflows/build-carts.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import os
+import re
+import subprocess
+import zipfile
+from pathlib import Path
+
+EXTS = {"lua", "moon", "wren", "js", "fnl"}
+META_PATTERN = re.compile(r"^(?:;;|\/\/|#|--) (\S+):\s*(.+)\s*$")
+REQ_METAS = {"title", "desc", "script"}
+
+
+def run_tic80_cart_cmd(cart: Path, cmd: str):
+    os.system(f'./.github/workflows/tic80 --fs=. --cli --cmd="load {cart} & ${cmd} & exit"')
+
+
+def build_cart(example: Path, cart: Path) -> bool:
+     # Get metadata from cart: title, author, desc, site, license, version, script
+    meta = {}
+    for line in subprocess.check_output(["strings", str(cart)]):
+        match = re.match(META_PATTERN, line)
+        if match:
+            meta[match.group(1)] = match.group(2)
+    # Check for required env vars
+    for req_meta in REQ_METAS:
+        if req_meta not in meta:
+            print(f"No title for {cart}")
+            return False
+
+    # Retry building HTML from cart (occasionally fails)
+    html_out = example / f"{meta['script']}.html.zip"
+    for i in range(5):
+        run_tic80_cart_cmd(cart, f"export html {example}/{meta['script']}.html")
+        if html_out.exists():
+            break
+    if not html_out.exists():
+        print(f"Failed to build {cart}!")
+        return False
+    out_dir = example / meta["script"]
+    out_dir.mkdir(parents=True)
+    with zipfile.ZipFile(html_out) as file:
+        file.extractall(out_dir)
+    html_out.unlink()
+
+    # Save PNG
+    run_tic80_cart_cmd(cart, f"save {example}/cart.png")
+
+    # Create post
+    with open(Path("_posts") / "template.md") as file:
+        contents = file.read().format(meta=meta, base=example.stem)
+    with open(Path("_posts") / f"2022-01-01-{example.stem}.md") as file:
+        file.write(contents)
+
+
+for example in Path("examples").iterdir():
+    if not example.is_dir():
+        continue
+    for ext in EXTS:
+        # Try both "src.moon" and "moon.tic"
+        cart = example / f"src.{ext}"
+        if cart.exists():
+            build_cart(example, cart)
+        else:
+            cart = example / f"{ext}.tic"
+            if cart.exists():
+                build_cart(example, cart)
--- _posts/template.md
+++ _posts/template.md
@@ -1,11 +1,11 @@
 ---
 layout: example
-title: ${TITLE}
-desc: ${DESC}
-tags: ${TAGS}
-lang: ${LANG}
-base: ${BASE}
+title: {meta["title"]}
+desc: {meta["desc"]}
+tags: {meta["tags"]}
+lang: {meta["script"]}
+base: {base}
 ---
 
-<iframe src="{{ site.baseurl }}/examples/${BASE}/${LANG}/index.html" title="Example cart"
+<iframe src="{{{{ site.baseurl }}}}/examples/{base}/{meta["script"]}/index.html" title="Example cart"
 width="720" height="408"></iframe>
